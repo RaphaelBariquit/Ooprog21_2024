b4b5733ddd998fef62ec07c9b90c0d6c7464f380
Raphael Bariquit <150428090+Raphlzx@users.noreply.github.com>
2024-08-14 23:55:11 +0800

    Add files via upload

---
+++ Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+MovieQuoteInfo.java
+public class MovieQuoteInfo { 
+    public static void displayQuoteInfo(){
+        String quote = "I shall return";
+        String movie = "Avengers";
+        String character = "Thor";
+        int year = 2012;
+        System.out.println("Quote: " + quote);
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    } 
+    public static void main(String[] args) {
+   displayQuoteInfo();
+    }
+}
