6a3931d47c2006e16c0abd71d2977b08986b695b
Raphael Bariquit <150428090+RaphaelBariquit@users.noreply.github.com>
2024-08-21 08:50:50 +0800

    Update MovieQuoteInfo.java

--- Chapter1/MovieQuoteInfo.java
+++ Chapter1/MovieQuoteInfo.java
@@ -2,9 +2,9 @@ MovieQuoteInfo.java
 public class MovieQuoteInfo { 
     public static void displayQuoteInfo(){
         String quote = "I knew it!";
-        String movie = "Avengers";
+        String movie = "Avengers End Game";
         String character = "Thor";
-        int year = 2012;
+        int year = 2019;
         System.out.println("Quote: " + quote);
         System.out.println("Movie: " + movie);
         System.out.println("Character: " + character);
