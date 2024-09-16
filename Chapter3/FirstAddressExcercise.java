14b11b3b72facfa5795e2db902fdafa4fc00099c
Raphael Bariquit <150428090+RaphaelBariquit@users.noreply.github.com>
2024-09-16 23:21:42 +0800

    Add files via upload

---
+++ Chapter3/FirstAddressExcercise.java
@@ -0,0 +1,12 @@
+class FirstAddressExercise{
+public static void displayAddress(){
+System.out.println("Xiaomi Corporation");
+System.out.println("29 West, Xinghuo Road, China");
+System.out.println("The Axis, Changning District");
+System.out.println("Company: Xiaomi Inc.");
+}
+public static void main(String[] args){
+displayAddress();
+
+}
+}
