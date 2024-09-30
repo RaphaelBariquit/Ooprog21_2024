06e6ced4163e80e07c942c6e7c7cd6d45f46a1dd
Raphael Bariquit <150428090+RaphaelBariquit@users.noreply.github.com>
2024-09-30 23:11:20 +0800

    Add files via upload

---
+++ Chapter3/Circle .java
@@ -0,0 +1,32 @@
+public class Circle {
+    private static double radius;
+    private static double diameter;
+    private static double area;
+
+    public Circle() {
+        radius = 1;
+        calculateDiameterAndArea();
+    }
+
+    public static void setRadius(double rad) {
+        radius = rad;
+        calculateDiameterAndArea();
+    }
+
+    private static void calculateDiameterAndArea() {
+        diameter = 2 * radius;
+        area = Math.PI * radius * radius;
+    }
+
+    public static double getRadius() {
+        return radius;
+    }
+
+    public static double getDiameter() {
+        return diameter;
+    }
+
+    public static double getArea() {
+        return area;
+    }
+}
